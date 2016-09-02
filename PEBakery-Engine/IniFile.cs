60f3deed3cd91abb70f2cc7684183d97bd111f81
Joveler <jb6804@naver.com>
2016-09-02 23:41:14 +0900

    Remove PowerCollections, Add SupportState.xlsx

--- PEBakery-Engine/IniFile.cs
+++ PEBakery-Engine/IniFile.cs
@@ -7,16 +7,16 @@ using System.IO;
 
 namespace BakeryEngine
 {
+    using IniDictionary = Dictionary<string, Dictionary<string, string>>;
     public class IniFile
     {
         private string iniFile;
-        
+        private IniDictionary key;
 
         public IniFile(string iniFile)
         {
             this.iniFile = iniFile;
-
-            
+            key = new IniDictionary();
         }
         
         public void ParseIni()
