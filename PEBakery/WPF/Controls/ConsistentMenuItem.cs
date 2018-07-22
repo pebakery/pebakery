cb4b755c880870ec1c378be62cfc158fc2d21002
Hajin Jang <jb6804@naver.com>
2018-07-23 00:26:17 +0900

    Another attempt to correct context menu icon size
    - Reference GitHub PR #67
    - Found Behaviors
    CorrectMenuItem.Icon is viewed under ContentPresenter.
    Win 7 and 10 has different default ContentPersenter.Margin value.
    
    Win 7  : 4, 0, 6, 0
    Win 10 : 3, 3, 3, 3

---
+++ PEBakery/WPF/Controls/ConsistentMenuItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using PEBakery.Core;
+
+namespace PEBakery.WPF.Controls
+{
+    public class CorrectMenuItem : MenuItem
+    {
+        public CorrectMenuItem()
+        {
+            Loaded += (object sender, RoutedEventArgs e) =>
+            {
+                if (!(Icon is FrameworkElement iconElement))
+                    return;
+                if (!(VisualTreeHelper.GetParent(iconElement) is ContentPresenter presenter))
+                    return;
+
+                App.Logger.SystemWrite(new LogInfo(LogState.Info, $"IconElement.Margin = {iconElement.Margin}"));
+                App.Logger.SystemWrite(new LogInfo(LogState.Info, $"Presenter.Margin   = {presenter.Margin}"));
+                iconElement.Width = Height;
+                iconElement.Height = Height;
+                iconElement.HorizontalAlignment = HorizontalAlignment.Center;
+                iconElement.VerticalAlignment = VerticalAlignment.Center;
+            };
+        }
+    }
+}
