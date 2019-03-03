7f6d39e83b2adc676f77cd5aa5f09b562b23e09f
Homes32 <firepark32@gmail.com>
2019-03-03 12:42:21 -0600

    App-wide "Windows like" SelectAll behavior for textboxes

--- PEBakery/App.xaml.cs
+++ PEBakery/App.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using PEBakery.Core;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PEBakery
 {
@@ -9,6 +11,39 @@ namespace PEBakery
         internal void App_Startup(object sender, StartupEventArgs e)
         {
             Global.PreInit(e.Args, false);
+            //RegisterTextBoxEvents();
         }
+
+        #region Event Handler
+        /// <summary>
+        /// Select all text in a TextBox control when the control gets the focus.
+        /// </summary>
+        protected void RegisterTextBoxEvents()
+        {
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseLeftButtonDownEvent,
+              new MouseButtonEventHandler(TextBoxHandleMouseButton), true);
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.GotKeyboardFocusEvent,
+              new RoutedEventHandler(TextBoxSelectAllText), true);
+        }
+
+        private static void TextBoxHandleMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            var textbox = (sender as TextBox);
+            if (textbox != null && !textbox.IsKeyboardFocusWithin)
+            {
+                if (e.OriginalSource.GetType().Name == "TextBoxView")
+                {
+                    e.Handled = true;
+                    textbox.Focus();
+                }
+            }
+        }
+
+        private static void TextBoxSelectAllText(object sender, RoutedEventArgs e)
+        {
+            if (e.OriginalSource is TextBox textBox)
+                textBox.SelectAll();
+        }
+        #endregion
     }
 }
