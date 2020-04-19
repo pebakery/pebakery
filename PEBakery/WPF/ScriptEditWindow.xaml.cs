pebakery/pebakery

commit 18f0ab125005295e9b02a48dcf19a7d8f58918d1

Enable deleting multiple interfaces at once

PEBakery/WPF/ScriptEditWindow.xaml.cs
@@ -2203,7 +2203,7 @@ namespace PEBakery.WPF
 
         private bool UICtrlDeleteCommand_CanExecute(object sender)
         {
-            return CanExecuteCommand && UICtrlEditEnabled;
+            return CanExecuteCommand && SelectedUICtrls != null && 0 < SelectedUICtrls.Count;
         }
 
         private void UICtrlDeleteCommand_Execute(object parameter)
@@ -2211,21 +2211,25 @@ namespace PEBakery.WPF
             CanExecuteCommand = false;
             try
             {
-                if (SelectedUICtrl == null)
+                if (SelectedUICtrls == null || SelectedUICtrls.Count == 0)
                     return;
 
-                UIControl uiCtrl = SelectedUICtrl;
-                UICtrlToBeDeleted.Add(uiCtrl);
+                UICtrlToBeDeleted.AddRange(SelectedUICtrls);
 
-                // Remove control's encoded file so we don't have orphaned Interface-Encoded attachments
-                DeleteInterfaceEncodedFile(uiCtrl);
+                foreach (UIControl uiCtrl in SelectedUICtrls)
+                {
+                    // Remove control's encoded file so we don't have orphaned Interface-Encoded attachments
+                    DeleteInterfaceEncodedFile(uiCtrl);
 
-                Renderer.UICtrls.Remove(uiCtrl);
+                    Renderer.UICtrls.Remove(uiCtrl);
+                }
+
                 InterfaceUICtrls = new ObservableCollection<string>(Renderer.UICtrls.Select(x => x.Key));
                 InterfaceUICtrlIndex = 0;
 
                 Renderer.Render();
                 SelectedUICtrl = null;
+                SelectedUICtrls = null;
 
                 WriteScriptInterface(null, true);
                 InterfaceNotSaved = true;

commit abd1b6e3a1da7107e7540d784f710fdef6356939

fixed regression caused by 18f0ab125005295e9b02a48dcf19a7d8f58918d1
The previous commit to enable multi-select delete of interface controls broke the ability to delete single-select controls. This commit corrects the issue and handles both single-select and multi-select delete properly.

PEBakery/WPF/ScriptEditWindow.xaml.cs
@@ -2203,7 +2203,7 @@ namespace PEBakery.WPF
 
         private bool UICtrlDeleteCommand_CanExecute(object sender)
         {
-            return CanExecuteCommand && SelectedUICtrls != null && 0 < SelectedUICtrls.Count;
+            return CanExecuteCommand && ((SelectedUICtrls != null && SelectedUICtrls.Count > 0) || (SelectedUICtrl != null));
         }
 
         private void UICtrlDeleteCommand_Execute(object parameter)
@@ -2211,17 +2211,35 @@ namespace PEBakery.WPF
             CanExecuteCommand = false;
             try
             {
-                if (SelectedUICtrls == null || SelectedUICtrls.Count == 0)
-                    return;
-
-                UICtrlToBeDeleted.AddRange(SelectedUICtrls);
-
-                foreach (UIControl uiCtrl in SelectedUICtrls)
+                if (SelectMode == ControlSelectMode.SingleSelect)
                 {
+                    // Single-Select
+                    if (SelectedUICtrl == null)
+                        return;
+
+                    UIControl uiCtrl = SelectedUICtrl;
+                    UICtrlToBeDeleted.Add(uiCtrl);
+
                     // Remove control's encoded file so we don't have orphaned Interface-Encoded attachments
                     DeleteInterfaceEncodedFile(uiCtrl);
 
                     Renderer.UICtrls.Remove(uiCtrl);
+                }
+                else if (SelectMode == ControlSelectMode.MultiSelect)
+                { 
+                    // Multi-Select
+                    if (SelectedUICtrls == null || SelectedUICtrls.Count == 0)
+                        return;
+
+                    UICtrlToBeDeleted.AddRange(SelectedUICtrls);
+
+                    foreach (UIControl uiCtrl in SelectedUICtrls)
+                    {
+                        // Remove control's encoded file so we don't have orphaned Interface-Encoded attachments
+                        DeleteInterfaceEncodedFile(uiCtrl);
+
+                        Renderer.UICtrls.Remove(uiCtrl);
+                    }
                 }
 
                 InterfaceUICtrls = new ObservableCollection<string>(Renderer.UICtrls.Select(x => x.Key));