e1f99cc3ed5a986ccf7ff9d48f154083b2a1a8f8
Hajin Jang <jb6804@naver.com>
2024-07-14 13:13:50 +0900

    Disable hack related to ShellExecuteEx
    Use ShellExecute instead of CreateProcess on running .exe files
    CreateProcess was used to counter bug on Win 11 + .NET 3.1.
    .NET 3.1 is discontinued and it is causing some .exe to fail on admin
    elevation. Disable the hack (@homes32)
    
    Need more testing on Windows 11, and .NET 8.

--- PEBakery.Core/Commands/CommandSystem.cs
+++ PEBakery.Core/Commands/CommandSystem.cs
@@ -579,15 +579,20 @@ namespace PEBakery.Core.Commands
                     // In Windows 11 + .NET Core 3.1, setting UseShellExecute = true causes exception.
                     // -> Win32Exception with "The operation was cancaled by the user" message
                     // To mitigate this, use CreateProcess instead of UseShellExecute on exe files.
-                    bool isExeFile = filePath.EndsWith(".exe", StringComparison.OrdinalIgnoreCase);
+                    //
+                    // => DISABLED hack as this causes admin elevation issues on some environments
+                    //
+                    // bool isExeFile = filePath.EndsWith(".exe", StringComparison.OrdinalIgnoreCase);
 
                     if (verb.Equals("Open", StringComparison.OrdinalIgnoreCase))
                     {
-                        proc.StartInfo.UseShellExecute = !isExeFile;
+                        // proc.StartInfo.UseShellExecute = !isExeFile;
+                        proc.StartInfo.UseShellExecute = true;
                     }
                     else if (verb.Equals("Min", StringComparison.OrdinalIgnoreCase))
                     {
-                        proc.StartInfo.UseShellExecute = !isExeFile;
+                        // proc.StartInfo.UseShellExecute = !isExeFile;
+                        proc.StartInfo.UseShellExecute = true;
                         proc.StartInfo.WindowStyle = ProcessWindowStyle.Minimized;
                     }
                     else if (verb.Equals("Hide", StringComparison.OrdinalIgnoreCase))
